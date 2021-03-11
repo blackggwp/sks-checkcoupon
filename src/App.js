ee23d4ab7cf3fb6e3b3f8492d746e7bcaddfbb9e
black <ironhighh@gmail.com>
2021-03-11 09:32:33 +0700

    change homepage

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-import React, { Suspense, lazy } from "react";
+import React, { Suspense, lazy } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
-} from "react-router-dom";
-import MyErrorBoundary from "./components/MyErrorBoundary";
-// import PrivateRoute from './helpers/PrivateRoute'
+} from 'react-router-dom'
+import MyErrorBoundary from './components/MyErrorBoundary';
 
-const Home = lazy(() => import("./pages/Home"));
-const Login = lazy(() => import("./pages/Login"));
+const Home = lazy(() => import('./pages/Home'));
+const Login = lazy(() => import('./pages/Login'));
 
 const App = () => {
   return (
-    <Router basename="/ecoupon">
-      {/* <Router> */}
+    <Router basename="/ecoupon/backend">
       <MyErrorBoundary>
         <Suspense fallback={<div>Loading...</div>}>
           <Switch>
-            {/* <PrivateRoute exact path="/" component={Home} /> */}
-            <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home} />
-            <Route path={`${process.env.PUBLIC_URL}/login`} component={Login} />
-            <Redirect to={`${process.env.PUBLIC_URL}/`} />
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Redirect to="/" />
           </Switch>
         </Suspense>
       </MyErrorBoundary>
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
